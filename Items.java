5adf528bcccc1a313768b08f33ddfcd94ae03f52
Alex <craciun43@gmail.com>
2020-01-06 13:56:31 +0100

    first commits

---
+++ Items.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Items here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Items
+{
+    private String itemName;
+    private String itemDescription;
+    private int itemAmount;
+    private int itemWeight;
+    /**
+     * Constructor for objects of class Items
+     */
+    public Items(String itemName, String itemDescription, int itemAmount, int itemWeight)
+    {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemAmount = itemAmount;
+        this.itemWeight = itemWeight;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private ArrayList<Items> inventory;
+    public Player(ArrayList<Items> inventory){
+        this.inventory = inventory;
+    }
+    
+}
